25cdd7fcd7aa32d970c46988c04979f4081aead4
Нарек Авагян <narekavagyan@MacBook-Pro-Narek.local>
2021-08-14 03:23:53 +0300

    closed db functions

--- db.controller.js
+++ db.controller.js
@@ -1,33 +1,33 @@
-const mongoose = require('mongoose');
-const cron = require('node-cron');
+// const mongoose = require('mongoose');
+// const cron = require('node-cron');
 
-const dotenv = require('dotenv');
-dotenv.config();
+// const dotenv = require('dotenv');
+// dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(process.env.MONGO_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
 
-const chatSchema = new mongoose.Schema({ chat_id: Number });
-const ChatModel = mongoose.model('Chat', chatSchema);
+// const chatSchema = new mongoose.Schema({ chat_id: Number });
+// const ChatModel = mongoose.model('Chat', chatSchema);
 
-const createNewChat = async (chatId) => {
-  const exists = await ChatModel.findOne({ chat_id: chatId });
-  if (!exists) {
-    ChatModel.create({ chat_id: chatId });
-  }
-};
+// const createNewChat = async (chatId) => {
+//   const exists = await ChatModel.findOne({ chat_id: chatId });
+//   if (!exists) {
+//     ChatModel.create({ chat_id: chatId });
+//   }
+// };
 
-cron.schedule('00 05 * * *', async () => {
-  const chatIds = (await ChatModel.find({})).map((item) => item.chat_id);
-  const randomJoke = await getContent(1);
-  chatIds.forEach((chatId) => {
-    bot.telegram.sendMessage(chatId, randomJoke);
-  });
-});
+// cron.schedule('00 05 * * *', async () => {
+//   const chatIds = (await ChatModel.find({})).map((item) => item.chat_id);
+//   const randomJoke = await getContent(1);
+//   chatIds.forEach((chatId) => {
+//     bot.telegram.sendMessage(chatId, randomJoke);
+//   });
+// });
 
-module.exports = { createNewChat };
+// module.exports = { createNewChat };
--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const { Telegraf, Markup } = require('telegraf');
 const ContentTypes = require('./content-types');
 const splitArrayToChunks = require('./heplers/array-to-chunk');
 const getContent = require('./heplers/request-content-api');
-const { createNewChat } = require('./db.controller');
+// const { createNewChat } = require('./db.controller');
 
 const bot = new Telegraf(process.env.BOT_API);
 const sections = splitArrayToChunks(Object.keys(ContentTypes));
 
 bot.start(async (ctx) => {
   ctx.reply(
-    'Жми че ты там хочешь и запомни че делать потому что эта инфа больше не появится. А ну еще я тебе каждый день в 5 утра буду присылать анекдот, чтобы ты просыпался (-ась господи) с улыбкой до ушей!!! 💖',
+    'Жми че ты там хочешь и запомни че делать потому что эта инфа больше не появится',
     Markup.keyboard(sections).resize()
   );
 
-  createNewChat(ctx.message.chat.id);
+  // createNewChat(ctx.message.chat.id);
 });
 
 bot.help((ctx) => ctx.reply('Боже я думаю ты сам разберешься че тут делать))))'));
